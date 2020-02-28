5a0e069681c5c887348c75c66f002a2036c867e7
Kevin Harrington <mad.hephaestus@gmail.com>
2020-02-28 12:52:01 -0500

    adding raw read example

--- gsonExample.groovy
+++ gsonExample.groovy
@@ -5,10 +5,21 @@ import java.lang.reflect.Type;
 
 String myURL = "https://gist.github.com/aa464b39c1208c357b8d4ae7fe210bbb.git"
 String filename = "data.json"
+//GSON parts
+Type TT_mapStringString = new TypeToken<HashMap<String, HashMap<String, Object>>>() {}.getType();
+Gson gson = new GsonBuilder().disableHtmlEscaping().setPrettyPrinting().create();
+ 
 // load daata from a Git json
 HashMap<String, HashMap<String, Object>> database = ScriptingEngine.gitScriptRun(
 											myURL,
 											filename,null)
+//Alternate method to load from a string
+//File	cachejson = ScriptingEngine.fileFromGit(myURL, filename)
+//String jsonString = null;
+//InputStream inPut = null;
+//inPut = FileUtils.openInputStream(cachejson);
+//jsonString = IOUtils.toString(inPut);											
+//HashMap<String, HashMap<String, Object>> database = gson.fromJson(StringVersionOfData, TT_mapStringString);
 // Reading Data
 for(String key:database.keySet()) {
 	HashMap<String, Object> data = database.get(key)											
@@ -25,8 +36,6 @@ newData.put("myNewKey","MyValue")
 newData.put("myOtherKey",37.42)
 database.put("NewDataset",newData)
 
-Type TT_mapStringString = new TypeToken<HashMap<String, HashMap<String, Object>>>() {}.getType();
- Gson gson = new GsonBuilder().disableHtmlEscaping().setPrettyPrinting().create();
  
 String writeOut = gson.toJson(database, TT_mapStringString);
 println "New database JSON content = \n\n"+writeOut
